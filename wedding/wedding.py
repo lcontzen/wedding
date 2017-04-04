1d485e4f18aa2d602c1f204f6ded5303f42ff419
Laurent Contzen <lcontzen@gmail.com>
2017-04-04 22:34:01 +0200

    Most of the visual

--- wedding/wedding.py
+++ wedding/wedding.py
@@ -4,12 +4,27 @@ from flask import Flask, request, session, g, redirect, url_for, \
 from flask_bootstrap import Bootstrap
 
 
-app = Flask(__name__)  # create the application instance :)
+app = Flask(__name__)
 Bootstrap(app)
-app.config.from_object(__name__)  # load config from this file , wedding.py
+app.config.from_object(__name__)
 app.config.from_envvar('WEDDING_SETTINGS', silent=True)
 
 
 @app.route('/')
 def show_index():
     return render_template('index.html')
+
+
+@app.route('/infos')
+def show_infos():
+    return render_template('infos.html')
+
+
+@app.route('/rsvp')
+def show_rsvp():
+    return render_template('rsvp.html')
+
+
+@app.route('/liste')
+def show_list():
+    return render_template('liste.html')
