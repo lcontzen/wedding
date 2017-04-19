8505d00425a2c689f976d4487c4b113b6bd39424
Laurent Contzen <lcontzen@gmail.com>
2017-04-19 22:53:10 +0200

    Changed logging level

--- wedding/wedding.py
+++ wedding/wedding.py
@@ -23,7 +23,7 @@ logging.basicConfig(filename='wedding/wedding.log',
                     level=logging.INFO,
                     format='%(asctime)s|%(levelname)s - %(message)s',
                     datefmt='%Y-%m-%d %H:%M:%S')
-logging.info("App started")
+logging.warning("App started")
 
 
 class Invited(db.Model):
